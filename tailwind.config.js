c39ffc9f1bda4442408bd89fc7bff97b97d902c4
Travis McMahon <azgoalie@gmail.com>
2021-12-10 00:06:01 -0700

    Upgraded tailwindcss to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{html,ts}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
         display: ["Cinzel Decorative", "cursive"],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
