5f1a4a02eb7dfd1386f3e1e0683bafe427848812
Travis McMahon <azgoalie@gmail.com>
2021-12-16 00:12:22 -0700

    Added @angula/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './navigation/navigation.module';
 import { AuthModule } from './auth/auth.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NavigationModule, AuthModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavigationModule,
+    AuthModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'elven-goods-market',
+    appId: '1:321215019645:web:65238f1a7303a9b7b50f14',
+    storageBucket: 'elven-goods-market.appspot.com',
+    apiKey: 'AIzaSyDBYMCplzkBxF4eN9EU8DwLpbLdYK8LAf8',
+    authDomain: 'elven-goods-market.firebaseapp.com',
+    messagingSenderId: '321215019645',
+  },
+  production: true,
 };
