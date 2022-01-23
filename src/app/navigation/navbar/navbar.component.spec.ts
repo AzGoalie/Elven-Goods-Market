f600c0fe2525cb0d3c17d6c953787338ac5679fb
Travis McMahon <azgoalie@gmail.com>
2022-01-23 15:56:02 -0700

    Added navbar tests

--- src/app/navigation/navbar/navbar.component.spec.ts
+++ src/app/navigation/navbar/navbar.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AuthService } from 'src/app/auth/auth.service';
+import { User } from '@angular/fire/auth';
+import { By } from '@angular/platform-browser';
+import { Subject } from 'rxjs';
+import { RouterLinkDirectiveStub } from '../../../testing';
+import { AuthService } from '../../auth/auth.service';
 import { NavbarComponent } from './navbar.component';
 
-const mockAuthService: Partial<AuthService> = {};
+const userSubject = new Subject<User>();
+const mockAuthService: Partial<AuthService> = {
+  user: userSubject,
+  signOut: () => Promise.resolve(),
+};
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
+  const getRouterLink = (innerText: string) => {
+    return fixture.debugElement
+      .queryAll(By.directive(RouterLinkDirectiveStub))
+      .find((de) => de.nativeElement.innerText.trim() === innerText)
+      ?.injector.get(RouterLinkDirectiveStub);
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [{ provide: AuthService, useValue: mockAuthService }],
-      declarations: [NavbarComponent],
+      declarations: [NavbarComponent, RouterLinkDirectiveStub],
     }).compileComponents();
   });
 
@@ -23,5 +38,56 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change links when signed in', () => {
+    fixture.detectChanges();
+    const dom: HTMLElement = fixture.nativeElement;
+    const navlinks = dom.querySelector('.navbar__links');
+
+    const signedOutLinks = ['Home', 'Sign Up', 'Sign In'];
+    expect(navlinks?.childElementCount).toBe(3);
+    Array.from(navlinks?.children!).forEach((element) => {
+      expect(signedOutLinks).toContain(element.textContent?.trim()!);
+    });
+
+    const signedInLinks = ['Home', 'Sign Out'];
+    userSubject.next({ uid: 'test user' } as User);
+    fixture.detectChanges();
+    expect(navlinks?.childElementCount).toBe(2);
+    Array.from(navlinks?.children!).forEach((element) => {
+      expect(signedInLinks).toContain(element.textContent?.trim()!);
+    });
+  });
+
+  it('should have a link to the sign up page', () => {
+    const routerLink = getRouterLink('Sign Up');
+    expect(routerLink?.linkParams).toBe('/signup');
+  });
+
+  it('should have a link to the sign in page', () => {
+    const routerLink = getRouterLink('Sign In');
+    expect(routerLink?.linkParams).toBe('/signin');
+  });
+
+  it('should have a link to the home page', () => {
+    const routerLink = getRouterLink('Home');
+    expect(routerLink?.linkParams).toBe('/');
+  });
+
+  it('should sign the user out', () => {
+    const authService = TestBed.inject(AuthService);
+    spyOn(authService, 'signOut');
+
+    userSubject.next({ uid: 'test user' } as User);
+    fixture.detectChanges();
+
+    const element = fixture.debugElement
+      .queryAll(By.directive(RouterLinkDirectiveStub))
+      .find((de) => de.nativeElement.innerText === 'Sign Out');
+
+    element?.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(authService.signOut).toHaveBeenCalled();
   });
 });
