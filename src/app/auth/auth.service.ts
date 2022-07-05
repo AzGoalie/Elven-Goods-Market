77511ae77d7816a20c3d12f6310378e1a6daa03d
Travis McMahon <azgoalie@gmail.com>
2022-07-05 16:51:32 -0700

    Revert "Stored auth in localstorage"
    This reverts commit 8947658efede81e0ee5a1f13d53a3fd787ccc051.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,20 +8,14 @@ import {
   User,
   UserCredential,
 } from '@angular/fire/auth';
+import {EMPTY, Observable} from 'rxjs';
+
 @Injectable()
 export class AuthService {
-  public user: User | null = null;
+  public user: Observable<User | null> = EMPTY;
 
   constructor(private auth: Auth) {
-    authState(this.auth).subscribe(user => {
-      if (user) {
-        this.user = user;
-        localStorage.setItem('user', JSON.stringify(user));
-      } else {
-        this.user = null;
-        localStorage.removeItem('user');
-      }
-    });
+    this.user = authState(this.auth);
   }
 
   public createAccount(email: string, password: string): Promise<UserCredential> {
@@ -34,9 +28,5 @@ export class AuthService {
 
   public signOut(): Promise<void> {
     return signOut(this.auth);
-  }
-
-  public isSignedIn(): boolean {
-    return !!localStorage.getItem('user');
   }
 }
